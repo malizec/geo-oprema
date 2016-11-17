47c6976bfe1ec0a549b1503f0757b756b28bd18e
Nenad Cvetkovic <nenadcv2@gmail.com>
2016-11-17 23:33:58 +0100

    Update config.php

--- _config/config.php
+++ _config/config.php
@@ -6,9 +6,3 @@ define('DB_HOST','localhost');
 define('DB_USER', 'root');
 define('DB_PASS','root');
 define('DATABASE', 'geooprema');
-
-/* online 
-define('DB_HOST','localhost');
-define('DB_USER', 'geooprem_1');
-define('DB_PASS','Nenad1Cvetkovic2');
-define('DATABASE', 'geooprem_db');*/
